80438759806cbc2c99fb52e5f5b769ef65e11001
Thiago de Jesus <99thiagodejesus@gmail.com>
2021-05-02 01:25:10 -0300

    Adicionada lógica da troca de icone

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ if (window.location.pathname == pathPaginaToons){
             input.focus()
         }
     }
-    window.localStorage.clear()
+
     // Se o usuário estiver vindo de outra página e algo tiver sido registrado na barra de pesquisa, lança um focus nela.
     if (input.value != "")
     {
@@ -79,3 +79,59 @@ function pesquisar (event) {
 }
 
 // ==== Fim mecanismo de busca ====
+
+//
+const avatar = document.querySelector("[data-avatar]")
+const divIcones = document.querySelector(["[data-icones]"])
+
+if (window.location.pathname == "/index.html")
+{
+    var icones = ["/imagens/avatar-icons/default-avatar.png", "/imagens/avatar-icons/power-girl.jpg", "/imagens/avatar-icons/arnold.jpg", "/imagens/avatar-icons/coragem.jpg"]
+    if (window.localStorage.getItem("avatar") != null)
+    {
+        const avatarPessoal = window.localStorage.getItem("avatar")
+        avatar.src = ".." + avatarPessoal
+    }
+} else {
+    var icones = ["../imagens/avatar-icons/default-avatar.png", "../imagens/avatar-icons/power-girl.jpg", "../imagens/avatar-icons/arnold.jpg", "../imagens/avatar-icons/coragem.jpg"]
+    if (window.localStorage.getItem("avatar") != null)
+    {
+        const avatarPessoal = window.localStorage.getItem("avatar")
+        avatar.src = avatarPessoal
+    }
+}
+
+divIcones.style.display = "none"
+avatar.addEventListener('click', () => {
+    if (divIcones.style.display == "none"){
+        divIcones.style.display = "grid"
+    } else {
+        divIcones.style.display = "none"
+    }
+})
+
+atualizarIcones()
+function atualizarIcones() {
+    for(var i = 0; i< icones.length; i++)
+    {
+        var element = document.createElement("li")
+        element.className = "icone"
+        var element2 = document.createElement("img")
+        element2.className = "icones"
+        element2.src = icones[i]
+        element2.addEventListener('click', trocarAvatar)
+        element.appendChild(element2)
+        divIcones.appendChild(element)
+    }
+}
+
+function trocarAvatar(element){
+    element = element.target
+    var temp = element.src
+    element = avatar.src
+    avatar.src = temp
+    window.localStorage.setItem("avatar", avatar.src.slice(avatar.src.indexOf("/imagens")))
+}
+
+window.localStorage.setItem("pesquisa", "")
+// 
