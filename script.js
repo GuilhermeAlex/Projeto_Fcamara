358956fa30f9c1467d6d03cf26edf1e0a683b8d2
Thiago de Jesus <99thiagodejesus@gmail.com>
2021-05-04 00:24:32 -0300

    Adicionado footer nas páginas

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ const input = document.querySelector("[data-search]")
 const pathPaginaToons = "/html/toons.html"
 
 // Como o mesmo script está sendo usado para mais de uma página, esse if declara as váriaveis da página toons apenas se o script for chamado a partir dela.
-if (window.location.pathname == pathPaginaToons){
+if (window.location.pathname == pathPaginaToons) {
     var h2Desenhos = document.querySelector("[data-desenhos]")
     var h2Acessados = document.querySelector("[data-h2-acessados]")
     var maisAcessados = document.querySelector("[data-acessados]")
     var desenhosElement = document.querySelector(".desenhos")
     var desenhosArray = desenhosElement.querySelectorAll(".item")
     var achados = 0
-    if (window.localStorage.getItem("pesquisa") != null)
-    {
-        if (window.localStorage.getItem("pesquisa").length <= 2)
-        {
+    if (window.localStorage.getItem("pesquisa") != null) {
+        if (window.localStorage.getItem("pesquisa").length <= 2) {
             input.value = window.localStorage.getItem("pesquisa")
         } else {
             input.focus()
@@ -22,54 +20,46 @@ if (window.location.pathname == pathPaginaToons){
     }
 
     // Se o usuário estiver vindo de outra página e algo tiver sido registrado na barra de pesquisa, lança um focus nela.
-    if (input.value != "")
-    {
+    if (input.value != "") {
         input.focus()
         pesquisar()
     }
 }
 
-
 // Adiciona um Event listener que aciona a função pesquisar sempre que é digitada uma letra nova
 input.addEventListener('keydown', pesquisar)
 input.addEventListener('keyup', pesquisar)
 
-function pesquisar (event) {
+function pesquisar(event) {
     // Verifica se o usuário se encontra na página do catalógo, caso não se encontre, encaminha para ela.
-    if (window.location.pathname != pathPaginaToons)
-    {
+    if (window.location.pathname != pathPaginaToons) {
         window.localStorage.setItem("pesquisa", event.key)
         return window.location.pathname = pathPaginaToons
     }
     const busca = input.value.toUpperCase();
     // Se a caixa de pesquisa tiver vazia ele restaura todos e retorna
-    if (input.value == '')
-    {
+    if (input.value == '') {
         h2Acessados.style.display = "block"
         maisAcessados.style.display = "grid"
         h2Desenhos.innerHTML = "Catálogo"
-        for (let i = 0; i < desenhosArray.length; i++)
-        {   
-        const elemento = desenhosArray[i]
-        elemento.style.display = "block"
+        for (let i = 0; i < desenhosArray.length; i++) {
+            const elemento = desenhosArray[i]
+            elemento.style.display = "block"
         }
-        return 
+        return
     }
     // Restaura todos os desenhos
-    for (let i = 0; i < desenhosArray.length; i++)
-    {   
+    for (let i = 0; i < desenhosArray.length; i++) {
         const elemento = desenhosArray[i]
         elemento.style.display = "block"
         achados++
     }
     // Esconde os que não estão de acordo com a pesquisa
-    for (let i = 0; i < desenhosArray.length; i++)
-    {   
+    for (let i = 0; i < desenhosArray.length; i++) {
         const elemento = desenhosArray[i]
         const nome = elemento.dataset.nome
         const genero = elemento.dataset.genero
-        if ((nome.toUpperCase().indexOf(busca) == -1) && (genero.toUpperCase().indexOf(busca) == -1))
-        {
+        if ((nome.toUpperCase().indexOf(busca) == -1) && (genero.toUpperCase().indexOf(busca) == -1)) {
             elemento.style.display = "none"
             achados--
         }
@@ -92,18 +82,15 @@ const avatar = document.querySelector("[data-avatar]")
 const divIcones = document.querySelector(["[data-icones]"])
 
 // Verifica se o script está sendo acessado a partir da página index, pois o caminho para as imagens a partir dela é diferente.
-if (window.location.pathname == "/index.html")
-{
+if (window.location.pathname == "/index.html") {
     var icones = ["/imagens/avatar-icons/default-avatar.png", "/imagens/avatar-icons/power-girl.jpg", "/imagens/avatar-icons/arnold.jpg", "/imagens/avatar-icons/coragem.jpg"]
-    if (window.localStorage.getItem("avatar") != null)
-    {
+    if (window.localStorage.getItem("avatar") != null) {
         const avatarPessoal = window.localStorage.getItem("avatar")
         avatar.src = ".." + avatarPessoal
     }
 } else {
     var icones = ["../imagens/avatar-icons/default-avatar.png", "../imagens/avatar-icons/power-girl.jpg", "../imagens/avatar-icons/arnold.jpg", "../imagens/avatar-icons/coragem.jpg"]
-    if (window.localStorage.getItem("avatar") != null)
-    {
+    if (window.localStorage.getItem("avatar") != null) {
         const avatarPessoal = window.localStorage.getItem("avatar")
         avatar.src = avatarPessoal
     }
@@ -114,8 +101,7 @@ if (window.location.pathname == "/index.html")
 atualizarIcones()
 // gera a lista de avatares disponíveis com base nos avatares registrados na lista icones
 function atualizarIcones() {
-    for(let i = 0; i< icones.length; i++)
-    {
+    for (let i = 0; i < icones.length; i++) {
         var element = document.createElement("li")
         element.className = "icone"
         var element2 = document.createElement("img")
@@ -129,7 +115,7 @@ function atualizarIcones() {
 
 // realiza a troca do avatar
 
-function trocarAvatar(element){
+function trocarAvatar(element) {
     element = element.target
     var temp = element.src
     element = avatar.src
@@ -142,12 +128,10 @@ function trocarAvatar(element){
 // ==== Acessados recentemente ====
 
 // Verifica se a página atual é a página do catálogo
-if (window.location.pathname == pathPaginaToons)
-{   
+if (window.location.pathname == pathPaginaToons) {
     // Verifica se há algum desenho recentemente acessado, se houver carrega eles.
     const acessadosRecente = document.querySelector("[data-acessados]")
-    if (window.localStorage.getItem("recentes") != null)
-    {
+    if (window.localStorage.getItem("recentes") != null) {
         var listaRecentes = JSON.parse(window.localStorage.getItem("recentes"))
         h2Acessados.style.display = "block"
 
@@ -155,19 +139,15 @@ if (window.location.pathname == pathPaginaToons)
         h2Acessados.style.display = "none"
         var listaRecentes = []
     }
-    
-    atualizaRecentesNaTela(listaRecentes)  
-    function atualizaRecentesNaTela(lista)
-    {
-        for(let i = 0; i < lista.length; i++)
-        {
+
+    atualizaRecentesNaTela(listaRecentes)
+    function atualizaRecentesNaTela(lista) {
+        for (let i = 0; i < lista.length; i++) {
             let desenhoInteresseNome = lista[i]
-            for (let c = 0; c < desenhosArray.length; c++)
-            {   
+            for (let c = 0; c < desenhosArray.length; c++) {
                 var elemento = desenhosArray[c]
                 const nome = elemento.dataset.nome
-                if (desenhoInteresseNome.toUpperCase() == nome.toUpperCase())
-                {
+                if (desenhoInteresseNome.toUpperCase() == nome.toUpperCase()) {
                     var elemento = elemento.cloneNode(true)
                     acessadosRecente.appendChild(elemento)
                 }
@@ -176,20 +156,16 @@ if (window.location.pathname == pathPaginaToons)
     }
 }
 
-
-function adicionaInteresse(element)
-{
+function adicionaInteresse(element) {
     // Verifica se o elemento clicado já está na lista de recentes
-    if (listaRecentes.indexOf(element.dataset.nome) == -1)
-    {
+    if (listaRecentes.indexOf(element.dataset.nome) == -1) {
         listaRecentes.unshift(element.dataset.nome)
     } else {
         // Se já estiver, muda a posição dele na lista para 0
         listaRecentes.splice(listaRecentes.indexOf(element.dataset.nome), 1)
         listaRecentes.unshift(element.dataset.nome)
     }
-    while(listaRecentes.length > 3)
-    {
+    while (listaRecentes.length > 3) {
         listaRecentes.pop()
     }
     // Atualiza a lista de recentes na memória local
